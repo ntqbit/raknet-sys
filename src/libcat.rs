ded3b26ffaba2bdedcd0afe38f1b5bcbc2c45cd0
ntqbit <ntqbit@proton.me>
2024-02-04 12:01:53 +0000

    Make libcat functions mutable

--- src/libcat.rs
+++ src/libcat.rs
@@ -42,7 +42,7 @@ impl AuthenticatedEncryption {
         }
     }
 
-    pub fn GenerateProof(&self, length: usize) -> Option<Vec<u8>> {
+    pub fn GenerateProof(&mut self, length: usize) -> Option<Vec<u8>> {
         let mut v: Vec<u8> = vec![0; length];
 
         let res = unsafe {
@@ -56,7 +56,7 @@ impl AuthenticatedEncryption {
         }
     }
 
-    pub fn ValidateProof(&self, proof: &[u8]) -> bool {
+    pub fn ValidateProof(&mut self, proof: &[u8]) -> bool {
         unsafe {
             ffi::AuthenticatedEncryption_ValidateProof(
                 self.inner,
@@ -66,7 +66,7 @@ impl AuthenticatedEncryption {
         }
     }
 
-    pub fn Encrypt(&self, msg: &[u8]) -> Option<Vec<u8>> {
+    pub fn Encrypt(&mut self, msg: &[u8]) -> Option<Vec<u8>> {
         let mut v: Vec<u8> = Vec::with_capacity(msg.len() + OVERHEAD_BYTES);
         v.write(msg).unwrap();
         v.write(&[0; OVERHEAD_BYTES]).unwrap();
@@ -90,7 +90,7 @@ impl AuthenticatedEncryption {
         }
     }
 
-    pub fn Decrypt(&self, msg: &[u8]) -> Option<Vec<u8>> {
+    pub fn Decrypt(&mut self, msg: &[u8]) -> Option<Vec<u8>> {
         let mut v = msg.to_vec();
         let mut msg_bytes = v.len() as u32;
 
@@ -126,7 +126,9 @@ impl ClientEasyHandshake {
         }
     }
 
-    pub fn GenerateServerKey(&self) -> Option<([u8; PUBLIC_KEY_BYTES], [u8; PRIVATE_KEY_BYTES])> {
+    pub fn GenerateServerKey(
+        &mut self,
+    ) -> Option<([u8; PUBLIC_KEY_BYTES], [u8; PRIVATE_KEY_BYTES])> {
         let mut public_key = [0; PUBLIC_KEY_BYTES];
         let mut private_key = [0; PRIVATE_KEY_BYTES];
 
@@ -145,12 +147,12 @@ impl ClientEasyHandshake {
         }
     }
 
-    pub fn Initialize(&self, public_key: &[u8; PUBLIC_KEY_BYTES]) -> bool {
+    pub fn Initialize(&mut self, public_key: &[u8; PUBLIC_KEY_BYTES]) -> bool {
         unsafe { ffi::ClientEasyHandshake_Initialize(self.inner, public_key.as_ptr() as _) }
     }
 
     pub fn SetIdentity(
-        &self,
+        &mut self,
         public_key: &[u8; PUBLIC_KEY_BYTES],
         private_key: &[u8; PRIVATE_KEY_BYTES],
     ) -> bool {
@@ -163,7 +165,7 @@ impl ClientEasyHandshake {
         }
     }
 
-    pub fn GenerateChallenge(&self) -> Option<[u8; CHALLENGE_BYTES]> {
+    pub fn GenerateChallenge(&mut self) -> Option<[u8; CHALLENGE_BYTES]> {
         let mut challenge: [u8; CHALLENGE_BYTES] = [0; CHALLENGE_BYTES];
         let res = unsafe {
             ffi::ClientEasyHandshake_GenerateChallenge(
@@ -179,7 +181,10 @@ impl ClientEasyHandshake {
         }
     }
 
-    pub fn ProcessAnswer(&self, answer: &[u8; ANSWER_BYTES]) -> Option<AuthenticatedEncryption> {
+    pub fn ProcessAnswer(
+        &mut self,
+        answer: &[u8; ANSWER_BYTES],
+    ) -> Option<AuthenticatedEncryption> {
         let authenticated_encryption = AuthenticatedEncryption::new();
         let res = unsafe {
             ffi::ClientEasyHandshake_ProcessAnswer(
@@ -225,7 +230,9 @@ impl ServerEasyHandshake {
         }
     }
 
-    pub fn GenerateServerKey(&self) -> Option<([u8; PUBLIC_KEY_BYTES], [u8; PRIVATE_KEY_BYTES])> {
+    pub fn GenerateServerKey(
+        &mut self,
+    ) -> Option<([u8; PUBLIC_KEY_BYTES], [u8; PRIVATE_KEY_BYTES])> {
         let mut public_key = [0; PUBLIC_KEY_BYTES];
         let mut private_key = [0; PRIVATE_KEY_BYTES];
 
@@ -245,7 +252,7 @@ impl ServerEasyHandshake {
     }
 
     pub fn Initialize(
-        &self,
+        &mut self,
         public_key: &[u8; PUBLIC_KEY_BYTES],
         private_key: &[u8; PRIVATE_KEY_BYTES],
     ) -> bool {
@@ -259,7 +266,7 @@ impl ServerEasyHandshake {
     }
 
     pub fn ProcessChallenge(
-        &self,
+        &mut self,
         challenge: &[u8; CHALLENGE_BYTES],
     ) -> Option<(AuthenticatedEncryption, [u8; ANSWER_BYTES])> {
         let mut answer: [u8; ANSWER_BYTES] = [0; ANSWER_BYTES];
